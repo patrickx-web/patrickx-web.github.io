b6eeca6baa72f1ee0377d81b1707743c71e29c07
Patrick <royebepusi@gmail.com>
2020-07-17 14:05:40 +0100

    Script.js
    Update

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-alert("hello!!!!!pick a gun of your choice and hit me up fr d party location☠💀 and go sanitize covid is real. thank u
-...codm fuckers")
+alert("hello!!!!!pick a gun of your choice and hit me up fr d party location☠💀 and go sanitize covid is real. thank u ...codm fuckers")
+
 
